7d89520a35663e09c3019c8420544a3150a01e28
robot <ci@rob.ot>
2021-11-03 13:03:59 +0000

    fixes from merge

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -7,116 +7,152 @@
  *  The save button sends the editor content to the same input name
  */
 
- import { FunctionalComponent } from "preact";
- import { useEffect } from "preact/hooks";
- import { Box, Flex, Spacer, VStack } from "@chakra-ui/react";
- import {
-   useHashParamJson,
-   useHashParamBase64,
-   useHashParamBoolean,
- } from "@metapages/metaframe-hook";
- import { Editor } from "/@/components/Editor";
- import { Option, OptionsMenuButton } from "/@/components/OptionsMenu";
- import { CodeResults } from "/@/components/CodeResults";
- import { Mode, useStore } from "/@/store";
- import { useExecuteCodeWithMetaframe } from "/@/hooks/useExecuteCodeWithMetaframe";
- import { ButtonRun } from "/@/components/ButtonRun";
- import { ButtonHelp } from "/@/components/ButtonHelp";
+import { FunctionalComponent } from "preact";
+import { useEffect } from "preact/hooks";
+import { Box, Flex, Spacer, VStack } from "@chakra-ui/react";
+import {
+  useHashParamJson,
+  useHashParamBase64,
+  useHashParamBoolean,
+} from "@metapages/metaframe-hook";
+import { Editor } from "/@/components/Editor";
+import { Option, OptionsMenuButton } from "/@/components/OptionsMenu";
+import { CodeResults } from "/@/components/CodeResults";
+import { Mode, useStore } from "/@/store";
+import { useExecuteCodeWithMetaframe } from "/@/hooks/useExecuteCodeWithMetaframe";
+import { ButtonRun } from "/@/components/ButtonRun";
+import { ButtonHelp } from "/@/components/ButtonHelp";
 
- const appOptions: Option[] = [
-   {
-     name: "theme",
-     displayName: "Light/Dark theme",
-     default: "light",
-     type: "option",
-     options: ["light", "vs-dark"],
-   },
- ];
+const appOptions: Option[] = [
+  {
+    name: "canvasWidth",
+    displayName: "Canvas width",
+    default: 300,
+    type: "number",
+  },
+  {
+    name: "canvasHeight",
+    displayName: "Canvas height",
+    default: 100,
+    type: "number",
+  },
+  {
+    name: "theme",
+    displayName: "Light/Dark theme",
+    default: "light",
+    type: "option",
+    options: ["light", "vs-dark"],
+  },
+  {
+    name: "stretch",
+    displayName: "Stretch to fill container",
+    default: false,
+    type: "boolean",
+  },
+];
 
- type OptionBlob = {
-   theme: string;
- };
+type OptionBlob = {
+  canvasWidth: number;
+  canvasHeight: number;
+  theme: string;
+  stretch: boolean;
+};
 
- export const Route: FunctionalComponent = () => {
-   // metaframe configuration
-   const [options] = useHashParamJson<OptionBlob>("options", {
-     theme: "light",
-   });
-   // presentation mode means hide all the editing stuff
-   const [isPresentationMode] = useHashParamBoolean("presentation");
-   const codeInStore = useStore((state) => state.code);
-   const setCodeInStore = useStore((state) => state.setCode);
-   const mode = useStore((state) => state.mode);
-   const setMode = useStore((state) => state.setMode);
-   const [runCode] = useExecuteCodeWithMetaframe();
+export const Route: FunctionalComponent = () => {
+  // metaframe configuration
+  const [options] = useHashParamJson<OptionBlob>("options", {
+    canvasWidth: 300,
+    canvasHeight: 100,
+    theme: "light",
+    stretch: false,
+  });
+  // presentation mode means hide all the editing stuff
+  const [isPresentationMode] = useHashParamBoolean("presentation");
+  const codeInStore = useStore((state) => state.code);
+  const setCodeInStore = useStore((state) => state.setCode);
+  const mode = useStore((state) => state.mode);
+  const setMode = useStore((state) => state.setMode);
+  const [runCode] = useExecuteCodeWithMetaframe();
 
-   // Split these next two otherwise editing is too slow as it copies to/from the URL
-   const [valueHashParam] = useHashParamBase64("text", undefined);
+  // Split these next two otherwise editing is too slow as it copies to/from the URL
+  const [valueHashParam] = useHashParamBase64("text", undefined);
 
-   // If state === Mode.Start, transition to Mode.Running.
-   // If there is no code, it's a no-op
-   useEffect(() => {
-     if (mode === Mode.Start) {
-       setMode(Mode.Running);
-     }
-   }, [mode, setMode]);
+  // If state === Mode.Start, transition to Mode.Running.
+  // If there is no code, it's a no-op
+  useEffect(() => {
+    if (mode === Mode.Start) {
+      setMode(Mode.Running);
+    }
+  }, [mode, setMode]);
 
-   // If state === Mode.Running, exec the code
-   useEffect(() => {
-     if (mode !== Mode.Running) {
-       return;
-     }
+  // If state === Mode.Running, exec the code
+  useEffect(() => {
+    if (mode !== Mode.Running) {
+      return;
+    }
 
-     // The code values are the same, but the user clicked the button, so execute
-     if (runCode) {
-       runCode(codeInStore);
-     }
-   }, [mode, codeInStore, runCode]);
+    // The code values are the same, but the user clicked the button, so execute
+    if (runCode) {
+      runCode(codeInStore);
+    }
+  }, [mode, codeInStore, runCode]);
 
-   // source of truth: the URL param #?text=<HashParamBase64>
-   // if that changes, set the local value
-   // the local value changes fast from editing
-   useEffect(() => {
-     setCodeInStore(valueHashParam);
-     if (runCode) {
-       runCode(valueHashParam);
-     }
-   }, [valueHashParam, setCodeInStore, runCode]);
+  // source of truth: the URL param #?text=<HashParamBase64>
+  // if that changes, set the local value
+  // the local value changes fast from editing
+  useEffect(() => {
+    setCodeInStore(valueHashParam);
+    if (runCode) {
+      runCode(valueHashParam);
+    }
+  }, [valueHashParam, setCodeInStore, runCode]);
 
-   if (isPresentationMode) {
-     return <div id="render" />;
-   }
+  const canvas = (
+    <canvas
+      style={
+        options?.stretch
+          ? { width: "100%", minWidth: options?.canvasWidth || 300 }
+          : undefined
+      }
+      width={options?.canvasWidth || 300}
+      height={options?.canvasHeight || 100}
+      id="renderCanvas"
+    />
+  );
+  if (isPresentationMode) {
+    return canvas;
+  }
 
-   const menu = (
-     <VStack spacing={2} alignItems="flex-start">
-       <Flex width="100%">
-         <ButtonRun />
-         <Spacer />
-         <ButtonHelp />
-         <OptionsMenuButton options={appOptions} />
-       </Flex>
-       <CodeResults />
-     </VStack>
-   );
+  const menu = (
+    <VStack spacing={2} alignItems="flex-start">
+      <Flex width="100%">
+        <ButtonRun />
+        <Spacer />
+        <ButtonHelp />
+        <OptionsMenuButton options={appOptions} />
+      </Flex>
+      <CodeResults />
+    </VStack>
+  );
 
-   return (
-     <Box w="100%" p={2}>
-       <VStack spacing={2} alignItems="flex-start">
-         {mode === Mode.Editing ? (
-           <Box w="100%">
-             <Editor
-               mode="javascript"
-               theme={options?.theme || "light"}
-               setValue={setCodeInStore}
-               value={codeInStore}
-             />
-           </Box>
-         ) : (
-           <div id="render" />
-         )}
+  return (
+    <Box w="100%" p={2}>
+      <VStack spacing={2} alignItems="flex-start">
+        {mode === Mode.Editing ? (
+          <Box w="100%">
+            <Editor
+              mode="javascript"
+              theme={options?.theme || "light"}
+              setValue={setCodeInStore}
+              value={codeInStore}
+            />
+          </Box>
+        ) : codeInStore && codeInStore.length > 0 ? (
+          canvas
+        ) : null}
 
-         <Box w="30%">{menu}</Box>
-       </VStack>
-     </Box>
-   );
- };
+        <Box w="30%">{menu}</Box>
+      </VStack>
+    </Box>
+  );
+};
